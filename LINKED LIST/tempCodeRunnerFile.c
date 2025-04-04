8ba170d2bb0fe9be1fded5241a79f3e27fbb56c9
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-04-04 12:08:32 +0530

    error

--- LINKED LIST/tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
-head = insertatEnd(head, 9);
-  printf("Linked List after insertion  \n");
-  linkedListTraversal(head);
--- tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
-  head = insertAtFirst(head, 56);
-//   printf("\n");
-//   linkedListTraversal(head);
